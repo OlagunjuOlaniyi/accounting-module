7a71f40d4b08afae3d62c2726fbe4738933efae2
Jibola <omojibolaorija@gmail.com>
2023-04-24 20:40:17 +0100

    .

--- src/hooks/queries/expenses.ts
+++ src/hooks/queries/expenses.ts
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query';
 import { getExpenses } from '../../services/expenseService';
 import { IexpenseRes } from '../../types/expenseTypes';
 
-//get billing cycles
+//get expenses
 export const useGetExpenses = () => {
   return useQuery<IexpenseRes>({
     queryKey: 'expenses',
